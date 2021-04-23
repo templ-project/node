ab62145447d440148075d6a8fb881dcdcbcee180
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-04-23 21:35:55 +0300

    refactor: attempt to add sonarqube to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,19 +11,28 @@ module.exports = {
   parserOptions: {ecmaVersion: 2018, sourceType: 'module'},
 
   // uncomment for eslint rules
-  extends: ['eslint:recommended', 'prettier', 'plugin:mocha/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:sonar/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:mocha/recommended',
+    'prettier',
+  ],
   // uncomment for airbnb rules
-  // extends: ['airbnb-base', 'prettier', 'plugin:jest/recommended'],
+  // extends: ['airbnb-base', 'plugin:sonar/recommended', 'plugin:jest/recommended', 'prettier'],
 
-  plugins: ['prettier'],
+  plugins: ['prettier', 'sonar', 'sonarjs'],
   root: true,
   rules: {
     'consistent-return': 2,
+    curly: ['error', 'all'],
     indent: [1, 2],
+    'max-len': ['error', 120],
     'max-lines-per-function': ['error', 20],
     'max-params': ['error', 3],
     'no-else-return': 1,
     semi: [1, 'always'],
+    'sonar/no-invalid-await': 0,
     'space-unary-ops': 2,
   },
 };
