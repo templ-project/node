231e88d4b6788c737411bfb99a736f831ad54ec9
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2021-04-23 22:01:42 +0300

    refactor: github actions for sonar

--- src/index.js
+++ src/index.js
@@ -4,54 +4,54 @@ module.exports = {
   hello,
 };
 
-// const curlyTest = (a, b, c, d) => {
-//   if (a < 0) a++;
-//   else console.log(foo);
+const curlyTest = (a, b, c, d) => {
+  if (a < 0) a++;
+  else console.log(foo);
 
-//   while (a === true) console.log('test');
+  while (a === true) console.log('test');
 
-//   for (let x of [a, b, c, d]) console.log(x);
-// };
+  for (let x of [a, b, c, d]) console.log(x);
+};
 
-// function returnValue(a) {
-//   // Noncompliant
-//   if (a === 1) {
-//     return true;
-//   }
+function returnValue(a) {
+  // Noncompliant
+  if (a === 1) {
+    return true;
+  }
 
-//   if (a.indexOf('blue') > 0) {
-//     return 'test';
-//   }
+  if (a.indexOf('blue') > 0) {
+    return 'test';
+  }
 
-//   switch (a) {
-//     case 0:
-//       doSomething();
-//       break;
-//     default:
-//       doSomethingElse();
-//       break;
-//   }
+  switch (a) {
+    case 0:
+      doSomething();
+      break;
+    default:
+      doSomethingElse();
+      break;
+  }
 
-//   return 3;
-// }
+  return 3;
+}
 
-// const eslintTest = async (a, b, c, d) => {
-//   console.log(a, b, c, d);
+const eslintTest = async (a, b, c, d) => {
+  console.log(a, b, c, d);
 
-//   curlyTest();
-//   returnValue();
+  curlyTest();
+  returnValue();
 
-//   {
-//     let x = undefined;
-//     if (x === NaN) {
-//       console.log('is NaN');
-//     }
-//   }
+  {
+    let x = undefined;
+    if (x === NaN) {
+      console.log('is NaN');
+    }
+  }
 
-//   {
-//     const x = 42;
-//     await x;
-//   }
+  {
+    const x = 42;
+    await x;
+  }
 
-//   alert('test');
-// };
+  alert('test');
+};
